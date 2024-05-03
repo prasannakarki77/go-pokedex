44c713a42af9ba8cd1992cd7db8f0ebc00875cfa
Prasanna Raj Karki <63549402+prasannakarki77@users.noreply.github.com>
2024-05-03 18:22:01 +0545

    start repl function

--- repl.go
+++ repl.go
@@ -16,9 +16,44 @@ func startRepl() {
 		scanner.Scan()
 		text := scanner.Text()
 
-		fmt.Println("echoing: ", text)
+		cleaned := cleanInput(text)
+		if len(cleaned) == 0 {
+			continue
+		}
+		commandName := cleaned[0]
+
+		availableCommands := getCommands()
+
+		command, ok := availableCommands[commandName]
+		if !ok {
+			fmt.Println("invalid command")
+			continue
+		}
+		command.callback()
+
 	}
 
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Prints the help menu",
+			callback:    callbackHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Turns off the Pokedex",
+			callback:    callbackExit,
+		},
+	}
 }
 
 func cleanInput(str string) []string {
